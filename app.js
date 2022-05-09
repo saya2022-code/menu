5feb192609e12129e000a9c0edd72db83bb3f15d
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-09 16:31:54 +0900

    node.js レッスンⅠ

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const app = express();
 // CSSや画像ファイルを置くフォルダを指定
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('hello.ejs');
-  });
 
-app.get('/top', (req, res) => {
+//トップ画面
+app.get('/', (req, res) => {
     res.render('top.ejs');
   });
+
+//一覧ページ
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+});
 
 // サーバーを起動するコードを貼り付けてください
 app.listen(3000);
