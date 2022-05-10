f4c68079cb1f0b1055a8c27ad4eaeb888bd42d5e
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-10 14:12:23 +0900

    DBのデータをejsに反映(途中)

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // expressを使うためのコード
 const express = require('express');
+// MySQLを使うためのコードを貼り付けてください
+const mysql = require('mysql');
+
 const app = express();
 
 // CSSや画像ファイルを置くフォルダを指定
 app.use(express.static('public'));
 
+// 定数connectionを定義して接続情報の書かれたコードを代入してください
+const connection = mysql.createConnection({
+  host: '',
+  user: 'todo2',
+  password: '!pass100',
+  database: 'list_app'
+});
 
 //トップ画面
 app.get('/', (req, res) => {
@@ -13,7 +23,14 @@ app.get('/', (req, res) => {
 
 //一覧ページ
 app.get('/index', (req, res) => {
-  res.render('index.ejs');
+  // データベースからデータを取得する処理を書いてください
+  connection.query(
+    'select * from items',
+    (error,results) => {
+      // console.log(results);
+      // res.renderの第２引数にオブジェクトを追加してください
+      res.render('index.ejs',{items:results});
+    });
 });
 
 // サーバーを起動するコードを貼り付けてください
