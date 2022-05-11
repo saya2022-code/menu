6805e02b5f465bb38f87bb2c8f0c6627c1e7c4de
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-11 18:49:32 +0900

    新規登録(バリデーションまで)

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(
 )
 
 //常に「セッション情報の一致」を確認するapp.useを作成
+    // (req,res,next)〜などの関数は全て「ミドルウェア関数」という
 app.use((req,res,next) => {
     // セッション情報のuserIdとundefinedを比較するif文を追加してください
     if(req.session.userId === undefined){
@@ -90,6 +91,68 @@ app.get('/', (req, res) => {
       });
     });
 
+    //新規登録へ遷移
+    app.get('/signup', (req, res) => {
+      res.render('signup.ejs',{errors: []}); //第2引数にエラー文の空の配列を追加
+    });
+
+    //新規登録の送信＝postなので画面遷移しない
+      //この機能は2つのミドルウェア関数(①、②)を持つ
+    app.post('/signup', (req, res, next) => {
+      //①フォームの入力をチェックする処理(バリデーション)＝next関数を追加
+      console.log('入力値の空チェック');
+
+      //登録フォームから送信された値を受け取る変数を用意(username,email,password)
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = req.body.password;
+ 
+      //先にエラー文を表示する空の配列を用意
+      const errors = [];
+      //値が空かを判定するには「' '（空文字列）」を使う。空なら配列(errors)にエラー文をpush
+      if(username === ''){
+        errors.push("ユーザー名が空です");
+      }
+      if(email === ''){
+        errors.push("メールアドレスが空です");
+      }
+      if(password === ''){
+        errors.push("パスワードが空です");
+      }
+      
+      console.log(errors);
+      //配列(errors)の長さで②に進むかの条件分岐
+      if(errors.length > 0){ //１以上の要素がある＝エラーあり
+        // res.redirect('/signup'); 登録画面に戻る
+        res.render('signup.ejs',{errors: errors}); //登録画面に戻り、signup.ejsにエラー文の入った配列errorsを渡す→signup.ejsへ
+  
+       }else{ //エラーなし
+      next(); //②へ進む
+      }
+    }, //ここまでが①
+    (req,res) => { 
+      //②新規登録の処理
+      console.log("新規登録");
+
+    //再度、登録フォームから送信された値を受け取る変数を用意(username,email,password)
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = req.body.password;
+
+    connection.query(
+      'INSERT INTO users (username,email,password) VALUES (?,?,?)',
+      [username,email,password], //変数
+      (error, results) => {
+        //セッション情報に加え、ログイン認証する
+        req.session.userId = results.insertId; //idカラムのみ、自動でresultsオブジェクトのinsertIdというプロパティに入る
+        req.session.username = username; //残りはフォーム送信された値を入れる(値はusernameという変数に代入されている)
+
+        // 一覧画面にリダイレクト＝重複を防ぐ
+        res.redirect('/index');
+      }
+    );
+  });
+
   //一覧表示
   app.get('/index', (req, res) => {
 
@@ -131,7 +194,7 @@ app.get('/', (req, res) => {
       );
       });
 
-  //作成画面
+  //作成画面への遷移
   app.get('/new', (req, res) => {
     res.render('new.ejs');
   });
